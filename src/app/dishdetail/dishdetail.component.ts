e0ee1a7afcc5e3f7ef1edce8bbd3dfda94d7eae4
Uwem Uke <slyart147@gmail.com>
2021-02-26 17:41:53 +0100

    assignment 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Dish } from '../shared/dish';
 
+import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
+
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +18,18 @@ export class HomeComponent implements OnInit {
 
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
 
-  constructor(private dishservice: DishService, private promotionservice: PromotionService) { }
+  constructor(
+  	private dishservice: DishService, 
+  	private promotionservice: PromotionService, 
+  	private leaderservice: LeaderService
+  ) { }
 
   ngOnInit() {
   	this.dish = this.dishservice.getFeaturedDish();
   	this.promotion = this.promotionservice.getFeaturedPromotion();
+  	this.leader = this.leaderservice.getFeaturedLeader();
   }
 
 }
